a5cfbfeb8e688cdf18657df6f95722aa145e4471
yusdi17 <yusdipathudin@gmail.com>
2025-04-14 17:15:40 +0700

    footer

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,42 +1,84 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
+</head>
+
+<body>
+
     <!-- Makanan -->
+    <div class="container px-4 py-5">
+        <h2 class="pb-2 border-bottom">Menu</h2>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            @foreach ($products as $product)
+                <div class="col">
+                    <div class="card shadow-sm">
+                        @if ($product->image)
+                            <img src="{{ asset('storage/' . $product->image) }}" class="bd-placeholder-img card-img-top"
+                                width="100%" height="225" style="object-fit: cover;">
+                        @endif
 
-<div class="container px-4 py-5">
-  <h2 class="pb-2 border-bottom">Menu</h2>
-  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-    @foreach ($products as $product)
-      <div class="col">
-        <div class="card shadow-sm">
-          @if ($product->image)
-            <img src="{{ asset('storage/' . $product->image) }}" class="bd-placeholder-img card-img-top" width="100%" height="225" style="object-fit: cover;">
-          @endif
-
-          <div class="card-body">
-            <p class="card-text">{{ $product->name }}</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-cart-plus"></i></button>
-              </div>
-              <small class="text-body-secondary">Rp {{ number_format($product->price, 0, ',', '.') }}</small>
-            </div>
-          </div>
+                        <div class="card-body">
+                            <p class="card-text">{{ $product->name }}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i
+                                            class="fa-solid fa-cart-plus"></i></button>
+                                </div>
+                                <small class="text-body-secondary">Rp
+                                    {{ number_format($product->price, 0, ',', '.') }}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
-      </div>
-    @endforeach
-  </div>
-</div>
+    </div>
+
+    <!-- Footer -->
+    <div class="container bg-body-tertiary">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top container">
+        <div class="col-md-4 d-flex align-items-center">
+            <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2025 Yusdi Fathudin</span>
+        </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3">
+                <a class="text-body-secondary" href="https://instagram.com/yuss.id17" target="_blank" aria-label="Instagram">
+                    <i class="fab fa-instagram" style="font-size: 1.5rem;"></i>
+                </a>
+            </li>
+            <li class="ms-3">
+                <a class="text-body-secondary" href="https://www.facebook.com/profile.php?id=100010838986673" target="_blank" aria-label="Facebook">
+                    <i class="fab fa-facebook" style="font-size: 1.5rem;"></i>
+                </a>
+            </li>
+            <li class="ms-3">
+                <a class="text-body-secondary" href="https://github.com/yusdi17" target="_blank" aria-label="Facebook">
+                    <i class="fa-brands fa-github" style="font-size: 1.5rem;"></i>
+                </a>
+            </li>
+            <li class="ms-3">
+                <a class="text-body-secondary" href="https://www.linkedin.com/in/yussd-id73/" target="_blank" aria-label="Facebook">
+                    <i class="fa-brands fa-linkedin"style="font-size: 1.5rem;"></i>
+                </a>
+            </li>
+        </ul>
+    </footer>
+    </div>
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
+    </script>
+</body>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
-  </body>
 </html>
