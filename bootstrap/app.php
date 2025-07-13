bbf6e3e94c5e0a3b52415b82db33a59ea547230c
yusdi17 <yusdipathudin@gmail.com>
2025-07-13 12:15:32 +0700

    midtrans callbcak

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(VerifyCsrfToken::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
